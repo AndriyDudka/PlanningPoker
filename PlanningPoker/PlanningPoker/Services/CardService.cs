75894d17030d3012b3bb2e1b80ab1f9768ff5f0c
Andriy <andriy.dudka@gmail.com>
2017-06-23 16:04:48 +0300

    Merge branch 'develop'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ PlanningPoker/PlanningPoker/Services/CardService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PlanningPoker.Models;
+
+namespace PlanningPoker.Services
+{
+    public class CardService
+    {
+        private List<Card> cards;
+
+        public CardService()
+        {
+            cards = new List<Card>
+            {
+                new Card{Id = 1, Name = "name1"},
+                new Card{Id = 2, Name = "name2"},
+                new Card{Id = 3, Name = "name3"},
+                new Card{Id = 4, Name = "name4"}
+            };
+        }
+
+        public List<Card> GetCards()
+        {
+            return cards;
+        }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public void DeleteCard(string name)
+        {
+            cards.Remove(cards.FirstOrDefault(x => x.Name.Equals(name)));
+        }
+    }
+}
